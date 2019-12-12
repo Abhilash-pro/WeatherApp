da5f28b9ac1ab18297c1c16511a9967cdecbde20
vermaabhi <abhilash.3580.verma@gmail.com>
2019-12-12 19:33:04 +0530

    1st component

---
+++ WeatherApp/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+	StyleSheet,
+	View,
+	Text,
+	TextInput,
+	KeyboardAvoidingView
+} from 'react-native';
+
+import SearchInput from './components/SearchInput';
+
+export default class App extends React.Component {
+
+  render(){
+    return (
+		
+		<KeyboardAvoidingView
+		style ={styles.container}
+		behavior = "padding"
+		>
+        <Text style = {styles.textStyle, styles.largeText}>
+			San Francisco
+         </Text>
+		 <Text style = {styles.textStyle, styles.smallText}>
+			Light Cloud
+         </Text>
+		 <Text style = {styles.textStyle, styles.largeText}>
+		 24°
+         </Text>
+		 <SearchInput placeholder = "Search any city"/>
+		 </KeyboardAvoidingView>
+     
+    );
+  };
+
+};
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ textStyle:{
+   textAlign:'center',
+   fontFamily: 'Roboto',
+ },
+ largeText:{
+	 fontSize: 44,
+ },
+ smallText:{
+	 fontSize : 18,
+ },
+ textInput:{
+	 backgroundColor: "#666",
+	 color:'white',
+	 height: 40,
+	 width: 300,
+	 marginTop: 20,
+	 marginHorizontal: 20,
+	 paddingHorizontal: 10,
+	 alignSelf: 'center',
+ },
+});
